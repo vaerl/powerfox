d1393ea6fc5e134e88a9e775acf82d3057b34a9e
vaerl <lukas@mlel.dev>
2023-05-19 23:15:18 +0200

    Update sql-schema.

--- sql/create.sql
+++ sql/create.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS config;
 DROP TABLE IF EXISTS days;
 
 CREATE TABLE config (
-    id serial PRIMARY KEY NOT NULL,
-    cost_heating numeric NOT NULL,
-    cost_general numeric NOT NULL,
-    monthly_budget_heating numeric NOT NULL,
-    monthly_budget_general numeric NOT NULL
+    id uuid PRIMARY KEY NOT NULL,
+    cost_heating double precision NOT NULL,
+    cost_general double precision NOT NULL,
+    monthly_budget_heating double precision NOT NULL,
+    monthly_budget_general double precision NOT NULL
 );
 
 CREATE TABLE days (
