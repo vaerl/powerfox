aed4e66af5f23cbe25e86fcf3a901ea1cb643f48
vaerl <lukas@mlel.dev>
2023-05-24 22:37:24 +0200

    Finalize systemd-script-stuff.

--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -1,7 +1,14 @@
+# symlink both timer and service so changes are immediately reflectedd (after systemctl daemon-reload)
 sudo ln -s /home/lukas/powerfox/scripts/powerfox-daily.service /etc/systemd/system/powerfox-daily.service
 sudo ln -s /home/lukas/powerfox/scripts/powerfox-daily.timer /etc/systemd/system/powerfox-daily.timer
 
-systemctl daemon-reload
+# restart systemd, necessary after changes to the timer/service
+sudo systemctl daemon-reload
 
-systemctl enable powerfox.timer
-systemctl start powerfox.timer
+# enable and start the timer which in turn triggers the service
+sudo systemctl enable powerfox-daily.timer
+sudo systemctl start powerfox-daily.timer
+
+# see the current status of both timer and service
+systemctl status powerfox-daily.timer
+systemctl status powerfox-daily.service
