996a00b6ed08ee249da1f340f5b847840f878a60
vaerl <lukas@mlel.dev>
2024-02-25 11:36:07 +0100

    Pass a network when running the container.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,7 +5,7 @@ docker build -t powerfox .
 docker build -t powerfox . --no-cache
 
 # run the image with the applicable environment
-docker run --name powerfox -d --env-file ./.env powerfox
+docker run --name powerfox -d --env-file ./.env --network=base powerfox
 
 # run bash inside the container
 docker exec -it powerfox bash
